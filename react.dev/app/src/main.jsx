0d7d1f012d87c10bf8aa813772846460715f5b59
lucassensei <Lucaswebdeveloper@proton.me>
2024-04-30 16:28:35 -0300

    Tic tac toe game finished. History feature left

---
+++ react.dev/app/src/main.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
